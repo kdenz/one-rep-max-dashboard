7119f91aa60c566113721688aeafb1172fa47c57
kdenz <ksnz93@gmail.com>
2019-11-06 00:46:28 +0800

    feat(OneRepMaxPage): add mocked side menu and exercise list

--- src/pages/OneRepMaxPage/OneRepMaxPage.tsx
+++ src/pages/OneRepMaxPage/OneRepMaxPage.tsx
@@ -1,11 +1,18 @@
+import { ExerciseList } from 'components/ExerciseList'
 import { SideMenu } from 'components/SideMenu'
+import { TopNavBar } from 'components/TopNavBar'
 import React from 'react'
 import styled from 'styled-components'
 
 const Container = styled.div`
   height: 100vh;
+  display: flex;
 `;
-const Content = styled.div``;
+const Content = styled.div`
+  background: ${p => p.theme.detailViewBg};
+  height: 100%;
+  width: 100%;
+`;
 
 // These ids are needed for side menu to work
 // https://github.com/negomi/react-burger-menu#properties
@@ -15,8 +22,31 @@ const PAGE_WRAP_ID = "one-rep-mac";
 export const OneRepMaxPage: React.FC = () => {
   return (
     <Container id={OUTER_CONTAINER_ID}>
-      <SideMenu pageWrapId={PAGE_WRAP_ID} />
-      <Content id={PAGE_WRAP_ID}>hi</Content>
+      <SideMenu
+        pageWrapId={PAGE_WRAP_ID}
+        outerContainerId={OUTER_CONTAINER_ID}
+        isDocked={true}
+        noOverlay={true}
+      >
+        <ExerciseList
+          title="Your exercises"
+          data={[
+            {
+              name: "Barbell Bench Press",
+              weight: 59.9,
+              unit: "lbs"
+            },
+            {
+              name: "Barbell Bench Press",
+              weight: 59.9,
+              unit: "lbs"
+            }
+          ]}
+        />
+      </SideMenu>
+      <Content id={PAGE_WRAP_ID}>
+        <TopNavBar title="Exercises" />
+      </Content>
     </Container>
   );
 };
