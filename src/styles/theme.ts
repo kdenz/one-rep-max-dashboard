7d1abcb10e894360c2e40bcd596f7fa7e6723ded
kdenz <ksnz93@gmail.com>
2019-11-06 00:45:36 +0800

    feat(theme): add new theme colors

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,5 +2,8 @@ export const theme = {
   primary: "#FF6E60",
   graph: "#4EC183",
   detailViewBg: "#242A2E",
-  menuBg: "#353533"
+  menuBg: "#353533",
+  white: "#fff",
+  grey: "rgba(255, 255, 255, 0.54)",
+  lightGrey: "rgba(255, 255, 255, 0.2)"
 };
