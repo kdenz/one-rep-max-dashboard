c828066e81ce20143a71d4b86364953627fce812
kdenz <ksnz93@gmail.com>
2019-11-06 00:44:13 +0800

    feat(SideMenu): implement a dockable side menu

--- src/components/SideMenu/SideMenu.tsx
+++ src/components/SideMenu/SideMenu.tsx
@@ -5,13 +5,27 @@ import styled from 'styled-components'
 const Container = styled.div`
   background: ${p => p.theme.menuBg};
   height: 100%;
+  min-width: 259px;
+  overflow: auto;
 `;
 
-export interface SideMenuProps extends MenuProps {}
-export const SideMenu: React.FC<SideMenuProps> = ({ ...menuProps }) => {
+export interface SideMenuProps extends MenuProps {
+  isDocked?: boolean;
+}
+export const SideMenu: React.FC<SideMenuProps> = ({
+  children,
+  isDocked,
+  ...menuProps
+}) => {
+  if (isDocked) return <Container>{children}</Container>;
   return (
-    <Menu {...menuProps}>
-      <Container>hi</Container>
+    <Menu
+      {...menuProps}
+      customBurgerIcon={false}
+      customCrossIcon={false}
+      isOpen={true}
+    >
+      <Container>{children}</Container>
     </Menu>
   );
 };
